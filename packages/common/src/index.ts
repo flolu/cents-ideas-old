03d41c9dbfba564d4b56855cce2e41de3ad091a8
skatestyle <fllu@protonmail.com>
2019-07-19 07:55:18 +0200

    base monorepo with yarn workspaces and typescript

---
+++ packages/common/src/index.ts
@@ -0,0 +1,2 @@
+export const centsMeaning: string[] = ['Control', 'Entry', 'Need', 'Time', 'Scale'];
+export const inventedBy: string = 'Mj Demarco';
---
+++ packages/gateway/src/index.ts
@@ -0,0 +1,3 @@
+import { centsMeaning, inventedBy } from '@cents-ideas/common';
+
+console.log('CENTS means: ', centsMeaning, ' and it was invented by ', inventedBy);
